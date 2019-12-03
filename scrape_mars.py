030c7b43200ebd88095c461353abd96e7a2b783c
Lindsey McKenna <lin.mckenna@gmail.com>
2019-12-03 12:19:40 -0800

    removed reference

--- scrape_mars.py
+++ scrape_mars.py
@@ -37,21 +37,3 @@ def scrape():
 
 if __name__ == "__main__":
     print(scrape())
-
-
-# def scrape():
-#     browser = init_browser()
-#     listings = {}
-
-#     url = "https://raleigh.craigslist.org/search/hhh?max_price=1500&availabilityMode=0"
-#     browser.visit(url)
-
-#     html = browser.html
-#     soup = BeautifulSoup(html, "html.parser")
-
-#     listings["headline"] = soup.find("a", class_="result-title").get_text()
-#     listings["price"] = soup.find("span", class_="result-price").get_text()
-#     listings["hood"] = soup.find("span", class_="result-hood").get_text()
-
-#     return listings
-
