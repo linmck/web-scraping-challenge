abee0cb9a03f5dfd449468978a833a2db923c725
Lindsey McKenna <lin.mckenna@gmail.com>
2019-12-03 12:18:18 -0800

    Updated scrape_mars with news and jpl image scrape

--- scrape_mars.py
+++ scrape_mars.py
@@ -9,27 +9,49 @@ def init_browser():
 
 
 def scrape():
-    nasa_mars_news_browser = init_browser()
-    story = {}
+    browser = init_browser()
+    news = {}
 
-    nasa_mars_news_url = "https://mars.nasa.gov/news/"
-    nasa_mars_news_browser.visit(nasa_mars_news_url)
+    news_url = "https://mars.nasa.gov/news/"
+    browser.visit(news_url)
 
-    nasa_mars_html = browser.html
-    news_soup = BeautifulSoup(nasa_mars_html, "html.parser")
+    news_html = browser.html
+    news_soup = BeautifulSoup(news_html, "html.parser")
 
-    story["title"] = news_soup.find("div", class_="content_title").get_text()
-    story["p"] = news_soup.find("div", class_="rollover_description").get_text()
-
-    jpl_browser = init_browser()
-    story = {}
+    news_html = news_soup.find('div', class_= 'slide')
+    news["title"] = news_html .find("div", class_="content_title").get_text().strip('\n')
+    news["p"] = news_html.find("div", class_="rollover_description").get_text().strip('\n')
 
     jpl_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
-    jpl_browser.visit(jpl_url)
+    browser.visit(jpl_url)
 
-    jpl_html = 
+    jpl = {}
+
+    jpl_html = browser.html
+    jpl_soup = BeautifulSoup(jpl_html, "html.parser")
+
+    jpl_html = (str(jpl_soup.find('article', class_= 'carousel_item')).split("background-image: url('/")[1]).split("')")[0]
+    jpl["ft_img_url"] = f'https://www.jpl.nasa.gov/{jpl_html}'
+
+    return news, jpl
+
+if __name__ == "__main__":
+    print(scrape())
 
 
-    return story
+# def scrape():
+#     browser = init_browser()
+#     listings = {}
 
+#     url = "https://raleigh.craigslist.org/search/hhh?max_price=1500&availabilityMode=0"
+#     browser.visit(url)
+
+#     html = browser.html
+#     soup = BeautifulSoup(html, "html.parser")
+
+#     listings["headline"] = soup.find("a", class_="result-title").get_text()
+#     listings["price"] = soup.find("span", class_="result-price").get_text()
+#     listings["hood"] = soup.find("span", class_="result-hood").get_text()
+
+#     return listings
 
